499f11717cf29fe7de6a6f1a6385032173120f30
Sudharsan25 <sudharsanskumar0125@gmail.com>
2025-10-07 22:11:07 +0530

    Fix: Removed Middleware temporarily due to Edge runtime limits

--- middleware.ts
+++ middleware.ts
@@ -1,35 +1,31 @@
-// middleware.ts
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+// // middleware.ts
+// import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     *
-     * This prevents the middleware from running on asset requests.
-     */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      *
+//      * This prevents the middleware from running on asset requests.
+//      */
+//     "/((?!_next/static|_next/image|favicon.ico).*)",
+//   ],
+// };
 
-const aj = arcjet({
-  key: process.env.ARCJET_KEY!,
-  rules: [
-    // Protect against common bots
-    shield({
-      mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
-      // Block all bots except the following
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-      ],
-    }),
-  ],
-});
+// const aj = arcjet({
+//   key: process.env.ARCJET_KEY!,
+//   rules: [
+//     detectBot({
+//       mode: "LIVE", // Blocks requests. Use "DRY_RUN" to log only
+//       // Block all bots except the following
+//       allow: [
+//         "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+//       ],
+//     }),
+//   ],
+// });
 
-export default createMiddleware(aj);
+// export default createMiddleware(aj);
